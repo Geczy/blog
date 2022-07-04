56bf1357a846a0880802b813add43b2fc8321ccd
Matt <Geczy@users.noreply.github.com>
2022-07-04 16:38:27 -0500

    unused navs

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,5 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/snippets', title: 'Snippets' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: '/uses', title: 'Uses' },
