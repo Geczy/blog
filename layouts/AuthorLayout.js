73e5974dc2505c4bbd89e4fe3771a28054507c20
Matt <Geczy@users.noreply.github.com>
2022-07-04 14:16:02 -0500

    add credit

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -174,8 +174,31 @@ export default function AuthorLayout({ children, frontMatter }) {
                         </g>
                       </svg>
                     </Link>
+                    : The original template that started it all.
+                  </li>
+                  <li>
+                    <Link
+                      href={'https://github.com/pycoder2000/blog'}
+                      className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
+                    >
+                      Path Desai's fork of the above template
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        className="ml-0.5 inline-block h-4 w-4 fill-current"
+                      >
+                        <g data-name="Layer 2">
+                          <g data-name="external-link">
+                            <rect width="24" height="24" opacity="0" />
+                            <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
+                            <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
+                          </g>
+                        </g>
+                      </svg>
+                    </Link>
                     : Template starter where I bootstrapped the project.
                   </li>
+
                   <li>
                     <Link
                       href={'https://www.einargudni.com/'}
