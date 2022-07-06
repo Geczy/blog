5660dc37b97ea268e4e85337aa11da4bb3c2e446
Matt <Geczy@users.noreply.github.com>
2022-07-05 19:40:31 -0500

    fix invalid provider type

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -26,7 +26,7 @@ const siteMetadata = {
     googleAnalyticsId: 'G-081Q6SPNH3', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
-    provider: 'emailOctopus',
+    provider: 'emailoctopus',
   },
   comment: {
     provider: 'giscus',
